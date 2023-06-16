45244cbd7aa4f29df77e8dc229ac2907538cb58d
phozpho <ajithrthampi999@gmail.com>
2023-06-17 00:42:38 +0530

    second commit

--- my-project/src/components/HeaderNavigate.jsx
+++ my-project/src/components/HeaderNavigate.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import {useNavigate} from "react-router-dom"
 import NewsFeeds from './NewsFeeds'
 import UpcomingEvents from './UpcomingEvents'
@@ -10,7 +10,6 @@ const HeaderNavigate = () => {
     const [loading, setLoading] = useState()
     const navigate = useNavigate()
 
- 
 
     const Data = [
         {
@@ -26,21 +25,26 @@ const HeaderNavigate = () => {
 
     ]
 
+
     const handleFeeds = (data) => {
-       localStorage.setItem('segments', data);  
-       setState(data)
+        localStorage.setItem('segments', data);
+        setState(data)
+        setLoading()
+
     }
+
 
     useEffect(() => {
         const storedSegmants = localStorage.getItem('segments');
         console.log(storedSegmants);
         setState(storedSegmants)
-    },[])
+    }, [])
 
    
+
     return (
         <>
-        
+
             <div className='flex md:gap-8 gap-3'>
 
                 {
@@ -49,21 +53,32 @@ const HeaderNavigate = () => {
                             item.id
                         }
                         className=''>
-                        <h2 className='text-[#6d6e71] font-bold font-Mulish md:text-base sm:text-sm text-xs cursor-pointer' onClick={() => handleFeeds(item.name)}>
+                        <h2 className='text-[#6d6e71] font-bold font-Mulish md:text-base sm:text-sm text-xs cursor-pointer'
+                            onClick={
+                                () => handleFeeds(item.name)
+                        }>
                             {
                             item ?. name
                         } </h2>
                     </div>
 
                 ))
-            } 
-            </div>
+            } </div>
             <hr className="h-[2px] md:my-4 my-4 bg-gray-400 border-0 dark:bg-gray-700"/>
-            <div>
-                {state === "News Feeds" && <NewsFeeds/>}
-                {state === "Update On Upcoming Events" && <UpcomingEvents/>}
-                {state === "Jobs" && <Jobs/>}
-                
+            <div> 
+            {
+                state === null && <NewsFeeds/>
+            }
+                {
+                state === "News Feeds" && <NewsFeeds/>
+            }
+                {
+                state === "Update On Upcoming Events" && <UpcomingEvents/>
+            }
+                {
+                state === "Jobs" && <Jobs/>
+            } 
+            
             </div>
 
         </>
